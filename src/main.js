f1e689894c9599cfe730b6fe4638653742fb98bc
miaoshudong <793126789@qq.com>
2022-05-15 09:33:44 +0800

    to 44 end

--- src/main.js
+++ src/main.js
@@ -2,28 +2,22 @@
 import Vue from 'vue'
 //引入App
 import App from './App.vue'
-//引入
-import VueRouter from 'vue-router'
-//引入路由器
-import router from './router/index'
+// 完整引入
+/* import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI) */
+
+import { Button, Row, DatePicker } from 'element-ui';
+Vue.component(Button.name, Button);
+Vue.component(Row.name, Row);
+Vue.component(DatePicker.name, DatePicker);
 //关闭Vue的生产提示
 Vue.config.productionTip = false
-Vue.use(VueRouter)
-//核心：解决代码冗余
-const originalPush = VueRouter.prototype.push
-VueRouter.prototype.push = function push(location) {
-	return originalPush.call(this, location).catch(err => err)
-}
-const originalReplace = VueRouter.prototype.replace
-VueRouter.prototype.replace = function replace(location) {
-	return originalReplace.call(this, location).catch(err => err)
-}
 //
 //创建vm
 new Vue({
 	el: '#app',
 	render: h => h(App),
-	router: router,
 	beforeCreate() {
 		Vue.prototype.$bus = this
 	}
